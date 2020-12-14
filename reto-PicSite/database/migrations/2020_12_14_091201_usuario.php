5f1c52bf7f9177bbc983eeb6deaebb30b8fe58a4
Endika <esanchezba1931@zubirimanteo.com>
2020-12-14 10:30:17 +0100

    migracion de la tabla usuario

---
+++ reto-PicSite/database/migrations/2020_12_14_091201_usuario.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Usuario extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('usuario', function (Blueprint $table) {
+            $table->id()->autoIncrement();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->string('nickname')->unique();
+            $table->string('contraseña');
+            $table->date('nacimiento');
+            $table->string('ciudad');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
